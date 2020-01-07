43c2a0ecfc00ea2aefbf2f9ec4e14398e11e1755
Romaric Thibault <torburma@gmail.com>
2020-01-07 16:49:26 +0100

    Added all themes colors to SCSS file

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,7 @@
-import "./index.sass"
+import "./index.scss"
 
 import $ from "jquery"
 
 $(window).scroll(function () {
-    $(".navbar").toggleClass("__home_navbar", $(this).scrollTop() > 0);
+    $(".navbar").toggleClass("__home_dark_navbar", $(this).scrollTop() > 0);
 });
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.sass$/i,
+                test: /\.scss$/i,
                 use: [
                     MiniCssExtractPlugin.loader,
                     "css-loader",
