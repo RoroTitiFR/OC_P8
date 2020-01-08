ef8b6784e65306bc5d1cba4bf52b1973d23d1350
Romaric Thibault <torburma@gmail.com>
2020-01-08 15:52:11 +0100

    Working search form, now requires the results page

--- app/views.py
+++ app/views.py
@@ -1,9 +1,40 @@
-from datetime import datetime
+from urllib.parse import quote
 
-from django.shortcuts import render
+import requests
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from app.forms.search import SearchForm
 
 
 def index(request):
+    if request.method == "POST":
+        form = SearchForm(request.POST)
+
+        if form.is_valid():
+            return redirect("/results/" + quote(form.data["search_term"]))
+
+    else:
+        form = SearchForm()
+
     return render(request, "app/index.html", {
-        "date": datetime.now()
+        "form": form
     })
+
+
+def results(request, search_term=""):
+    if search_term == "":
+        return redirect("/")
+
+    request_url = "https://fr.openfoodfacts.org/cgi/search.pl?" \
+                  "action=process&" \
+                  f"search_terms={search_term}&" \
+                  "sort_by=unique_scans_n&" \
+                  "page_size=20&" \
+                  "action=display&" \
+                  "json=1"
+
+    r = requests.get(request_url)
+    json = r.json()
+
+    return HttpResponse(json)
