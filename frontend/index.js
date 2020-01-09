7eb8fd0afd05beeae1bc712d31e03d92f51d3c21
Romaric Thibault <torburma@gmail.com>
2020-01-09 11:45:13 +0100

    Responsive navbar and animation

--- frontend/index.js
+++ frontend/index.js
@@ -4,5 +4,23 @@ import "./tailwind.scss"
 import $ from "jquery"
 
 $(window).scroll(function () {
-    $(".navbar").toggleClass("__home_dark_navbar", $(this).scrollTop() > 0);
+    const navbar = $(".navbar");
+    navbar.toggleClass("__home_dark_navbar", $(this).scrollTop() > 0);
+});
+
+$(document).ready(function () {
+    const navbar = $(".navbar");
+    const navbarMenu = $(".navbar-menu");
+    const navbarBurger = $(".navbar-burger");
+
+    // Check for click events on the navbar burger icon
+    navbarBurger.click(function () {
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        navbarBurger.toggleClass("is-active");
+        navbarMenu.toggleClass("is-active");
+
+        if ($(window).scrollTop() === 0) {
+            navbar.toggleClass("__home_dark_navbar")
+        }
+    });
 });
