834819b212ba0aee5b71c2ede59daf6a5d0f096d
Romaric Thibault <torburma@gmail.com>
2020-01-13 09:30:17 +0100

    Postgres Data model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    code = models.TextField(primary_key=True)
+    name = models.TextField()
+
+    nutrition_score = models.IntegerField()
+    nutrition_grade = models.CharField(max_length=1)
+
+    energy_100g = models.IntegerField()
+    energy_unit = models.TextField()
+    carbohydrates_100g = models.FloatField()
+    sugars_100g = models.FloatField()
+    fat_100g = models.FloatField()
+    saturated_fat_100g = models.FloatField()
+    salt_100g = models.FloatField()
+    sodium_100g = models.FloatField()
+    fiber_100g = models.FloatField()
+    proteins_100g = models.FloatField()
+
+    thumbnail_url = models.TextField()
+
+
+class Category(models.Model):
+    code = models.TextField(primary_key=True)
+    name = models.TextField()
+
+
+class CategoryProduct(models.Model):
+    id_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    id_product = models.ForeignKey(Product, on_delete=models.CASCADE)
--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'pur_beurre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'pur_beurre',
+        'USER': 'pur_beurre',
+        'PASSWORD': 'pur_beurre',
+        'HOST': '192.168.1.230'
     }
 }
 
