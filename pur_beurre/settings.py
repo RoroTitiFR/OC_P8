ad5f8a0a6247d3bdaead961cd5cbdc9665f86df0
Romaric Thibault <torburma@gmail.com>
2020-01-22 16:22:44 +0100

    Heroku settings

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,3 +128,7 @@ AUTH_USER_MODEL = 'app.PurBeurreUser'
 
 LOGIN_URL = "/login"
 LOGIN_REDIRECT_URL = "/"
+
+# Heroku: Update database configuration from $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
